9ea9b2195da5f3978013d708885f9ffd89bc1f7a
Jana E. Beck <jana.eliz.beck@gmail.com>
2012-04-16 01:43:07 -0400

    modified script to export _stats.csv by default, _bubble.csv with -b option

--- dexcom_stats.py
+++ dexcom_stats.py
@@ -5,11 +5,15 @@ def main():
 
     parser = argparse.ArgumentParser(description='Process the input files.')
     parser.add_argument('-d', '--dexcom', action = 'store', dest = "dex_name", help='name of Dexcom xml file')
+    parser.add_argument('-b', '--bubble', action = 'store_true', dest = "bubble", help='produce bubble chart .csv')
     args = parser.parse_args()
 
     d = Dexcom(args.dex_name)
 
-    d.stats()
+    if args.bubble:
+    	d.bubble_chart()
+    else:
+    	d.stats()
 
 if __name__ == "__main__":
     main()
