ac2f99e249966c1c7e98728e183187a731a9a713
Jana E. Beck <jana.eliz.beck@gmail.com>
2013-01-19 22:58:58 -0500

    added a script to combine .csv exports from different Dexcom receivers into one .csv file

---
+++ util/combine.py
@@ -0,0 +1,49 @@
+import csv
+import datetime
+
+def main():
+
+	output = csv.writer(open('all_dexcom.csv', 'w'))
+
+	header = ['timestamp', 'blood_glucose']
+
+	output.writerow(header)
+	
+	with open('../raw/receiver1.csv', 'rb') as f:
+		rdr = csv.reader(f, delimiter = '\t', quoting = csv.QUOTE_NONE)
+		rdr.next()
+		
+		for row in rdr:
+			row_to_write = []
+			pair = row[3].split('.')
+			time = datetime.datetime.strptime(pair[0], "%Y-%m-%d %H:%M:%S")
+			row_to_write.append(time.strftime("%Y-%m-%d %H:%M:%S"))
+			row_to_write.append(row[4])
+			output.writerow(row_to_write)
+
+	with open('../raw/receiver2.csv', 'rb') as f:
+		rdr = csv.reader(f, delimiter = '\t', quoting = csv.QUOTE_NONE)
+		rdr.next()
+		
+		for row in rdr:
+			row_to_write = []
+			pair = row[3].split('.')
+			time = datetime.datetime.strptime(pair[0], "%Y-%m-%d %H:%M:%S")
+			row_to_write.append(time.strftime("%Y-%m-%d %H:%M:%S"))
+			row_to_write.append(row[4])
+			output.writerow(row_to_write)
+
+	with open('../raw/receiver3.csv', 'rb') as f:
+		rdr = csv.reader(f, delimiter = '\t', quoting = csv.QUOTE_NONE)
+		rdr.next()
+		
+		for row in rdr:
+			row_to_write = []
+			pair = row[3].split('.')
+			time = datetime.datetime.strptime(pair[0], "%Y-%m-%d %H:%M:%S")
+			row_to_write.append(time.strftime("%Y-%m-%d %H:%M:%S"))
+			row_to_write.append(row[4])
+			output.writerow(row_to_write)
+		
+if __name__ == "__main__":
+    main()
