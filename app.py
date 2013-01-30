b60a4fa7b80fea5c98e30d651f1f92688c94245a
Jana E. Beck <jana.eliz.beck@gmail.com>
2013-01-30 03:00:02 -0500

    preliminary steps towards uploading and processing input files

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 import cherrypy
 
 class App():
-	@cherrypy.expose
-	def index(self):
-		return open('assets/html/index.html')
+    @cherrypy.expose
+    def index(self):
+        return open('assets/html/index.html')
+
+    def upload(self, dex, ping, yfd):
+        out = """<html>
+        <body>
+            dex length: %s<br />
+            dex filename: %s<br />
+            dex mime-type: %s
+        </body>
+        </html>"""
+
+        # Although this just counts the file length, it demonstrates
+        # how to read large files in chunks instead of all at once.
+        # CherryPy reads the uploaded file into a temporary file;
+        # dex.file.read reads from that.
+        size = 0
+        while True:
+            data = dex.file.read(8192)
+            if not data:
+                break
+            size += len(data)
+
+        return out % (size, dex.filename, dex.content_type)
+    upload.exposed = True
 
 if __name__ == '__main__':
 
