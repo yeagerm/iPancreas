bbea30b5501aba5d5828609fe6d4feeaffef127b
Jana E. Beck <jana.eliz.beck@gmail.com>
2013-01-31 00:41:21 -0500

    updated dexcom_stats.py to use new file passing instead of filename passing

--- dexcom_stats.py
+++ dexcom_stats.py
@@ -1,5 +1,8 @@
-from churn_data import Dexcom
 import argparse
+import csv
+
+from churn_data import Dexcom
+from dexcom_g4_importer import G4Reader as G4
 
 def main():
 
@@ -8,6 +11,17 @@ def main():
     args = parser.parse_args()
 
     d = Dexcom(args.dex_name)
+
+    if args.dex_name.find('.xml') != -1:
+        d.file = open(args.dex_name, 'rU')
+        d.readings = d.get_readings()
+        d.ext = '.xml'
+    else:
+        platinum = G4(csv.reader(open(args.dex_name, 'rb'), delimiter='\t', quoting = csv.QUOTE_NONE))
+        d.readings = platinum.readings
+        d.ext = '.csv'
+
+    d._make_stats_files()
 
     d.stats()
     d.day_csv()
