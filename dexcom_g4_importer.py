49337e637cd183fcd898fc462dde8c3357fab011
Jana E. Beck <jana.eliz.beck@gmail.com>
2013-01-31 00:29:58 -0500

    merged G4 importer script from master branch

---
+++ dexcom_g4_importer.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import csv
+
+class G4Reader():
+	"""Replicates the functionality of Dexcom.get_readings() in churn_data."""
+
+	def __init__(self, dex_name):
+		
+		with open(dex_name, 'rb') as f:
+			self.rdr = csv.reader(f, delimiter = '\t', quoting = csv.QUOTE_NONE)
+			self.readings = self.get_readings()
+
+	def get_readings(self):
+
+		readings = []
+
+		self.rdr.next()
+
+		for row in self.rdr:
+			soup = BeautifulSoup("<Sensor></Sensor>", "xml")
+			reading = soup.Sensor
+			reading['DisplayTime'] = row[3]
+			reading['Value'] = row[4]
+			if reading['Value'] == "Low":
+				reading['Value'] = 39
+			elif reading['Value'] == "High":
+				reading['Value'] = 401
+			readings.append(reading)
+
+		return readings
+
+		
+
+	
+
+
